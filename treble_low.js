45f487383d06c840bf617447fcd0b9c6c7dff2c0
toshiko-lab <tonton@mvd.biglobe.ne.jp>
2026-05-29 13:12:25 +0900

    Update treble_low.js

--- treble_low.js
+++ treble_low.js
@@ -36,7 +36,7 @@ function draw() {
   text("ト音記号 ト～1点ト", width / 2, 55);
 
   // ト音記号
-　image(clefImg, -10, 280, 110, 160);
+　image(clefImg, -10, 190, 110, 160);
   drawStaff();
   drawNote();
   drawKeyboard();
