7ce266c02aa9b6dee408b663b66630c03b9b80ee
toshiko-lab <tonton@mvd.biglobe.ne.jp>
2026-06-10 23:04:34 +0900

    Update sound_test.js

--- bass_e_to_e/sound_test.js
+++ bass_e_to_e/sound_test.js
@@ -1,15 +1,13 @@
-let synth;
-
 function setup() {
   createCanvas(400, 200);
-  synth = new p5.MonoSynth();
 }
 
 function draw() {
   background(220);
+
   textAlign(CENTER, CENTER);
   textSize(24);
-  text("画面をタップ", width/2, height/2);
+  text("画面をタップ", width / 2, height / 2);
 }
 
 function mousePressed() {
@@ -17,6 +15,13 @@ function mousePressed() {
   userStartAudio();
   getAudioContext().resume();
 
-  synth.play("C4", 0.8, 0, 0.5);
+  let osc = new p5.Oscillator('sine');
 
+  osc.freq(440);
+  osc.start();
+  osc.amp(0.5, 0.05);
+
+  setTimeout(() => {
+    osc.stop();
+  }, 500);
 }
