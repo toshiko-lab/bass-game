2f7f756a608f7cdf293c14b0863609d47820b2d7
toshiko-lab <tonton@mvd.biglobe.ne.jp>
2026-07-02 14:20:23 +0900

    Update game_master.js

--- game_master.js
+++ game_master.js
@@ -1,6 +1,4 @@
 let currentNote;
-let score = 0;
-let mistakes = 0;
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
@@ -10,39 +8,24 @@ function setup() {
 function draw() {
   background(220);
   
-  // 1. 五線譜と音符の描画
-  drawStaff();
-  if (currentNote) {
-    drawNote(currentNote);
-  }
-  
-  // 2. 鍵盤の描画
-  drawKeyboard();
-}
-
-function drawStaff() {
+  // 1. 五線譜の描画
   stroke(0);
   for (let i = 0; i < 5; i++) {
     line(50, 200 + i * 20, windowWidth - 50, 200 + i * 20);
   }
-}
-
-function drawNote(noteName) {
-  let note = config.noteData.find(n => n.name === noteName);
-  if (note) {
+  
+  // 2. 音符の描画（configのY座標をそのまま使う）
+  let noteObj = config.noteData.find(n => n.name === currentNote);
+  if (noteObj) {
     fill(0);
-    ellipse(windowWidth / 2, note.y, 20, 15);
+    ellipse(windowWidth / 2, noteObj.y, 25, 20);
   }
-}
-
-function drawKeyboard() {
-  // 白鍵の描画
-  fill(255);
-  stroke(0);
+  
+  // 3. 鍵盤の描画
+  fill(255); stroke(0);
   for (let k of config.keys) {
     rect(k.x, 400, k.w, 100);
   }
-  // 黒鍵の描画
   fill(0);
   for (let bk of config.blackKeys) {
     rect(bk.x, 400, bk.w, 60);
@@ -54,15 +37,10 @@ function newQuestion() {
 }
 
 function mousePressed() {
-  // 鍵盤が押されたかの判定（簡易版）
   for (let k of config.keys) {
     if (mouseX > k.x && mouseX < k.x + k.w && mouseY > 400 && mouseY < 500) {
       if (k.note === currentNote) {
-        score++;
         console.log("正解！");
-      } else {
-        mistakes++;
-        console.log("間違い");
       }
       newQuestion();
       break;
