a204520250851dbb742cfb2a58869c27e29b80f6
toshiko-lab <tonton@mvd.biglobe.ne.jp>
2026-07-02 11:29:53 +0900

    Update config_bass.js

--- config_bass.js
+++ config_bass.js
@@ -1,5 +1,20 @@
-currentConfig = {
+config = {
+  title: "ヘ音記号 ト～ト",
   clefImg: "bass.png",
-  noteYMap: { "G": 300, "A": 290, ... }, // ヘ音のデータ
-  keys: [ {note: "G", x: 40, y: 520, w: 50, h: 120}, ... ]
+  clefX: 50, clefY: 185, clefW: 80, clefH: 135,
+  startY: 300, timeLimit: 60,
+  noteData: [
+    {name: "G", y: 260}, {name: "A", y: 270}, {name: "B", y: 280}, {name: "C", y: 290},
+    {name: "D", y: 300}, {name: "E", y: 310}, {name: "F", y: 320}, {name: "G_high", y: 330}
+  ],
+  keys: [
+    {note: "G", x: 100, w: 70}, {note: "A", x: 170, w: 70}, {note: "B", x: 240, w: 70},
+    {note: "C", x: 310, w: 70}, {note: "D", x: 380, w: 70}, {note: "E", x: 450, w: 70},
+    {note: "F", x: 520, w: 70}, {note: "G_high", x: 590, w: 70}
+  ],
+  blackKeys: [
+    {note: "F#", x: 91, w: 20}, {note: "G#", x: 120, w: 40}, {note: "A#", x: 190, w: 40},
+    {note: "C#", x: 330, w: 40}, {note: "D#", x: 400, w: 40}, {note: "F#_middle", x: 540, w: 40},
+    {note: "F#_high", x: 609, w: 20}
+  ]
 };
