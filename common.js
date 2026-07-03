dbb15514a80435cb015c47bc14891c0d28e9fd1b
toshiko-lab <tonton@mvd.biglobe.ne.jp>
2026-07-03 12:52:59 +0900

    Update common.js

--- common.js
+++ common.js
@@ -3,13 +3,15 @@ function drawKeyboard() {
   for (let k of config.keys) {
     fill(255);
     stroke(0);
-    rect(k.x, 450, k.w, 120);
+    strokeWeight(2);
+    rect(k.x, 430, k.w, 140); // 押しやすいように少し縦長に
   }
 
   // 黒鍵の描画
   fill(0);
   noStroke();
   for (let b of config.blackKeys) {
-    rect(b.x, 450, b.w, 80);
+    // config.blackKeysに定義された座標（b.x）を使って正確に描画
+    rect(b.x, 430, b.w, 85);
   }
 }
